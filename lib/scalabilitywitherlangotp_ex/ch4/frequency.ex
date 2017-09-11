91438f0651e41e82fa6dcb6718eaff565f537a2c
Ricardo García Vega <bigardone@gmail.com>
2017-09-11 17:41:29 +0200

    ch4 frequency gen_server implementation and tests

---
+++ lib/scalabilitywitherlangotp_ex/ch4/frequency.ex
@@ -0,0 +1,95 @@
+defmodule ScalabilitywitherlangotpEx.Ch4.Frequency do
+  @moduledoc """
+  https://github.com/francescoc/scalabilitywitherlangotp/blob/master/ch4/frequency.erl
+  """
+  use GenServer
+
+  ####################
+  # CLIENT FUNCTIONS #
+  ####################
+
+  @doc """
+  Starts the frequency server. Called by supervisor
+  """
+  def start do
+    GenServer.start_link(__MODULE__, [], name: :frequency)
+  end
+
+  @doc """
+  Stops the frequency server
+  """
+  def stop do
+    GenServer.cast(:frequency, :stop)
+  end
+
+  @doc """
+  If available, it returns a frequency used to make a call.
+  Frequency must be deallocated on termination.
+  """
+  def allocate do
+    GenServer.call(:frequency, {:allocate, self()})
+  end
+
+  @doc """
+  Frees a frequency so it can be used by another client
+  """
+  def deallocate(frequency) do
+    GenServer.cast(:frequency, {:deallocate, frequency})
+  end
+
+  ######################
+  # CALLBACK FUNCTIONS #
+  ######################
+
+  @doc """
+  Initialises the generic server by getting the list of
+  available frequencies. [] is the list of allocated ones
+  """
+  def init(_args) do
+    frequencies = {get_frequencies(), []}
+
+    {:ok, frequencies}
+  end
+
+  def handle_call({:allocate, pid}, _from, frequencies) do
+    {new_frequencies, reply} = allocate(frequencies, pid)
+
+    {:reply, reply, new_frequencies}
+  end
+
+  def handle_cast({:deallocate, freq}, frequencies) do
+    new_frequencies = deallocate(frequencies, freq)
+
+    {:noreply, new_frequencies}
+  end
+
+  def handle_cast(:stop, frequencies), do: {:stop, :normal, frequencies}
+
+  def handle_info(_msg, frequencies), do: {:noreply, frequencies}
+
+  # Termination callback. Does nothing, but should instead kill clients
+  def terminate(_reason, _frequencies), do: :ok
+
+  def format_status(_opt, [_pro_dict, {available, allocated}]) do
+    {:data, [{"State", {{:available, available}, {:allocated, allocated}}}]}
+  end
+
+  ######################
+  # INTERNAL FUNCTIONS #
+  ######################
+
+  defp get_frequencies, do: [10, 11, 12, 13, 14, 15]
+
+  defp allocate({[], allocated}, _pid) do
+    {{[], allocated}, {:error, :no_frequency}}
+  end
+  defp allocate({[res | resouces], allocated}, pid) do
+    {{resouces, [{res, pid} | allocated]}, {:ok, res}}
+  end
+
+  defp deallocate({free, allocated}, res) do
+    new_allocated = List.keydelete(allocated, res, 1)
+
+    {[res | free], new_allocated}
+  end
+end
---
+++ test/scalabilitywitherlangotp/ch4/frequency_test.exs
@@ -0,0 +1,56 @@
+defmodule ScalabilitywitherlangotpEx.Ch4.FrequencyTest do
+  use ExUnit.Case, async: true
+
+  alias ScalabilitywitherlangotpEx.Ch4.Frequency
+
+  setup do
+    Frequency.start()
+
+    on_exit fn ->
+      Frequency.stop()
+    end
+
+    :ok
+  end
+
+  describe "start/0" do
+    test "starts new frequency process" do
+      :frequency
+      |> Process.whereis
+      |> Process.alive?
+      |> assert
+    end
+  end
+
+  describe "stop/0" do
+    test "ends process loop" do
+      assert :ok = Frequency.stop()
+      Process.sleep(1)
+      refute GenServer.whereis(:frequency)
+
+      Frequency.start()
+    end
+  end
+
+  describe "allocate/0" do
+    test "returns a free frequency when available" do
+      Enum.each(10..15, &(assert {:ok, &1} = Frequency.allocate()))
+    end
+
+    test "returns error when no free frequencies" do
+      Enum.each(10..15, fn(_) -> Frequency.allocate() end)
+
+      assert {:error, :no_frequency} = Frequency.allocate()
+    end
+  end
+
+  describe "deallocate/1" do
+    test "frees the given frequency" do
+      {:ok, frequency} = Frequency.allocate()
+
+      assert :ok = Frequency.deallocate(frequency)
+
+      assert {:ok, ^frequency} = Frequency.allocate()
+    end
+  end
+end
